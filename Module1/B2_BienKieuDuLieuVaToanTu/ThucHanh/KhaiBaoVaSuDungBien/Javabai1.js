dcb458ad44fe99067341c8fab545f86bc1f4945c
NguyenSyMong <nsm100299@gmail.com>
2019-12-26 11:33:43 +0700

    Cap Nhat Bai Tap Bai 2

---
+++ Module1/B2_BienKieuDuLieuVaToanTu/ThucHanh/KhaiBaoVaSuDungBien/Javabai1.js
@@ -0,0 +1,13 @@
+let f = 20.5
+let i = 10
+let b = true
+let s = 'Hà Nội'
+
+
+document.write('f = ' + f);
+document.write('<br/>');
+document.write('i = ' + i + "<br>");
+document.write('b = ' + b);
+document.write('<br/>')
+document.write('s = ' + s+ "<br>");
+
